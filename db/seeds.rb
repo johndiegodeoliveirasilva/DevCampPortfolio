12a9cbe1fd2e4ec7bcf2138b39b610954fcd9a4f
johndiego <johnoliveirasilva9@gmail.com>
2020-10-30 17:08:11 -0300

    Implemened custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." 
+    body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." ,
+    topic_id: Topic.last.id
   )
 end
 
@@ -26,7 +33,18 @@ puts "5 skills created"
 9.times do |portfolio_item|
   Portfolio.create!(
      title: "Portfolio title: #{portfolio_item}" ,
-     subtitle: "My great service",
+     subtitle: "Ruby on Rails",
+     body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." ,
+     main_image: "http://placehold.it/600x400" ,
+     thumb_image: "http://placehold.it/350x200"
+  )
+end
+puts "9 postfolio items created"
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+     title: "Portfolio title: #{portfolio_item}" ,
+     subtitle: "Angular",
      body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." ,
      main_image: "http://placehold.it/600x400" ,
      thumb_image: "http://placehold.it/350x200"
