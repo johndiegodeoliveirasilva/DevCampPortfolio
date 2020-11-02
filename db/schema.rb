34188ce3c89c9a3c4279b7de6354d88fc6bd97dd
JOHN DIEGO <johnoliveirasilva9@gmail.com>
2020-11-01 23:35:48 -0300

    Integrated technologies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_175220) do
+ActiveRecord::Schema.define(version: 2020_11_01_182149) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -53,11 +53,19 @@ ActiveRecord::Schema.define(version: 2020_11_01_175220) do
     t.text "badge"
   end
 
+  create_table "technologies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.bigint "portfolio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
+  end
+
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "blogs", "topics"
+  add_foreign_key "technologies", "portfolios"
 end
