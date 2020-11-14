3216f10bbeaa0e62a53dd029e638fe327ebb7c79
JOHN DIEGO <johnoliveirasilva9@gmail.com>
2020-11-13 23:52:54 -0300

    Added nav bar and added default hearder content

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   
   get 'contact', to: 'pages#contact'
   
