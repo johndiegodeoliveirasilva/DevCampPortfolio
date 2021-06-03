ffa9a8eacfa3dc2a2a724552ff6bc487285fb0c2
John Diego <johnoliveirasilva9@gmail.com>
2021-06-03 11:25:27 -0300

    Added Sum Ingredient plus 20 percent

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,13 @@
 class Dish < ApplicationRecord
+  before_save :sum_ingredient
+
   has_one_attached :avatar
   has_and_belongs_to_many :ingredients
+
+  private
+
+  def sum_ingredient
+    sum = ingredients.map(&:price).sum
+    self.total = sum + (sum * 0.2)
+  end
 end
