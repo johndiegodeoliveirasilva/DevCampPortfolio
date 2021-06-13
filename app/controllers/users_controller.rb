8c79b0bdcb5cb09f97e70269208936365e9bbfbb
John Diego <johnoliveirasilva9@gmail.com>
2021-06-13 14:53:51 -0300

    Added Controller Users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class ApplicationController < ApplicationController::Base
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @dish = Dish.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+    
+  end
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :roles, :active?)
+  end
+end
