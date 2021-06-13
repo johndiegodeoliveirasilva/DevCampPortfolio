87ac3a1f774c50e6e56e47a20ea4f1e22144ced6
John Diego <johnoliveirasilva9@gmail.com>
2021-06-13 14:15:55 -0300

    Wrong Identation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
     end 
   end
   def copyright_generator
-     DevcampViewTool::Renderer.copyright 'John Diego', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'John Diego', 'All rights reserved'
   end
 
   def nav_items
